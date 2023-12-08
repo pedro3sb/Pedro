406cc35b540a5db38d50df7015099c88c35bf488
pedrosoaresbarbosa05@gmail.com <you@example.com>
2023-12-07 21:07:23 -0300

    first commit

---
+++ src/ph/cabecalho.java
@@ -0,0 +1,18 @@
+package ph;
+
+public class cabecalho {
+    public static String Faculdade = "Faculdade: Faculdade Alfredo Nasser";
+    public static String Aluno = "Aluno: Pedro Henrique Soares Barbosa";
+    public static String Professor = "Professor: Brenno Pimenta";
+    public static String Disciplina = "Disciplina: Algoritmo e Programaçao II";
+    public static String Inicio = "Bem Vindo ao Quiz!";
+    public void escrevaCabecalho(){
+
+        System.out.println(this.Faculdade);
+        System.out.println(this.Aluno);
+        System.out.println(this.Professor);
+        System.out.println(this.Disciplina);
+        System.out.println(this.Inicio);
+        
+    }
+}
